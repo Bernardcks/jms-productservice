71795ad48001586256abe4038701d4c9120a28d6
Bernardcks <bernardchua.2024@computing.smu.edu.sg>
2026-03-02 13:59:39 +0800

    chore: set up json response error handlers for not found and on error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
 
 const app = new OpenAPIHono();
 
+app.notFound(notFound);
+app.onError(onError);
+
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+// Fake error route for testing error handling
+app.get("/error", (c) => {
+  c.status(418);
+  throw new Error("This is a test error");
 });
 
 export default app;
