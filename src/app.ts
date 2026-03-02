5f79f3843127da20539886dcafeceea538338d36
Bernardcks <bernardchua.2024@computing.smu.edu.sg>
2026-03-02 14:14:16 +0800

    feat: added pino logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { notFound, onError } from "stoker/middlewares";
+import { pinoLoggerWrapper } from "./middlewares/pino-logger.js";
 
 const app = new OpenAPIHono();
 
+app.use(pinoLoggerWrapper());
 app.notFound(notFound);
 app.onError(onError);
 
