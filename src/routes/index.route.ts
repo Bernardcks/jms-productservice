dcb23438d19dca9543cb2922a497d106b656b42e
Bernard Chua <bernardchua.2024@computing.smu.edu.sg>
2026-03-08 16:25:01 +0800

    feat: refactor routes to use stoker helpers

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,9 +1,12 @@
 import { createRoute, z } from "@hono/zod-openapi";
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import { jsonContent } from "stoker/openapi/helpers";
+import { createMessageObjectSchema } from "stoker/openapi/schemas";
 import { createRouter } from "@/lib/create-app.js";
 
 // Flow for implementing routes
 // 1. Define contract
-// 2. Define implementation
+// 2. Define implementation{
 
 const router = createRouter()
   .openapi(
@@ -11,22 +14,16 @@ const router = createRouter()
       method: "get",
       path: "/",
       responses: {
-        200: {
-          content: {
-            "application/json": {
-              schema: z.object({
-                message: z.string(),
-              }),
-            },
-          },
-          description: "Listing API Index",
-        },
+        [HttpStatusCodes.OK]: jsonContent(
+          createMessageObjectSchema("Listings API"),
+          "Listing API Index",
+        ),
       },
     }),
     (c) => {
       return c.json({
         message: "Listings API",
-      });
+      }, HttpStatusCodes.OK);
     },
   );
 
