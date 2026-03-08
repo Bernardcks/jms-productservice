cea7fcca5b8c5d78080ee9f5df508738e1c6263f
Bernard Chua <bernardchua.2024@computing.smu.edu.sg>
2026-03-08 16:29:30 +0800

    feat: add listings route to the application

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import configureOpenAPI from "@/lib/configure-open-api.js";
 import createApp from "@/lib/create-app.js";
 import index from "@/routes/index.route.js";
+import listings from "@/routes/listings/listings.index.js";
 
 const app = createApp();
 
 const routes = [
   index,
+  listings,
 ];
 
 configureOpenAPI(app);
