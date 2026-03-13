d0edf798953a2f4089c3c24898778f8f59e867c9
Bernard Chua <bernardchua.2024@computing.smu.edu.sg>
2026-03-13 11:22:18 +0800

    feat: added config type for rabbitmq

--- src/lib/rabbitmq/config.ts
+++ src/lib/rabbitmq/config.ts
@@ -2,5 +2,9 @@ import env from "@/env";
 
 export const rabbitConfig = {
   url: env.RABBITMQ_URL,
-  exchange: env.RABBITMQ_EXCHANGE
-}
+  exchange: env.RABBITMQ_EXCHANGE,
+  exchangeType: "topic" as const,
+  queue: env.RABBITMQ_QUEUE,
+  routingKeys: ["listings.#"] as const,
+  prefetchL: env.RABBITMQ_PREFETCH,
+} as const;
